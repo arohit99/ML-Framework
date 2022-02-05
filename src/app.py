4ade6b38895b8ebe4d28d558db49b381c6cb84c9
Rohit <arohitreddy99@gmail.com>
2022-02-05 16:56:49 -0500

    updates

--- src/app.py
+++ src/app.py
@@ -1,8 +1,15 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.model_selection import cross_val_score
 import sys,os
+
+PROJECTPATH = 'C:\\Users\\rohit\\Downloads\\mh\\workspace\\ML-Framework'
+
+if PROJECTPATH not in sys.path:
+    sys.path.append(PROJECTPATH)
+
+from sklearn.model_selection import cross_val_score
+
 from pathlib import Path
 import logging
 #import library.components as lib
--- src/test.py
+++ src/test.py
@@ -1,13 +1,32 @@
-import sys
-from pathlib import Path
+# import sys
+# from pathlib import Path
 
-PROJECTPATH = Path(__file__).parent.parent.absolute()
+#PROJECTPATH = Path(__file__).parent.parent.absolute()
+PROJECTPATH = 'C:\\Users\\rohit\\Downloads\\mh\\workspace\\ML-Framework'
+
+# if PROJECTPATH not in sys.path:
+#     sys.path.append(PROJECTPATH)
+
+# print(sys.path)
+
+# import streamlit as st
+
+# st.title('Customer Churn Model')
+
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import cross_val_score
+import sys,os
+from pathlib import Path
+import logging
+import mlflow, mlflow.sklearn
 
 if PROJECTPATH not in sys.path:
     sys.path.append(PROJECTPATH)
 
-print(sys.path)
-
-import streamlit as st
-
-st.title('Customer Churn Model')
+#import library.components as lib
+from library.components import Data, Model
+from library.custom_preprocess import extract_features
+from config import config as cf
+from pages import DataView,PlotView,ModelMetrics
